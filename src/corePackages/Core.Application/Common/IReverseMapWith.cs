0588b25eb00e8cfaa4d2bbd7032a3a214f917d90
Onur Çelik <180101013@ogrenci.yalova.edu.tr>
2022-09-04 17:12:29 +0300

    corePackages

---
+++ src/corePackages/Core.Application/Common/IReverseMapWith.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Application.Common
+{
+    public interface IReverseMapWith<T>
+    {
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType()).ReverseMap();
+    }
+   
+}
